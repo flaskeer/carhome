bc1e4ccea5eda52091d0be0cb71c07b5bfaf27d2
flaskeer <1185098948@qq.com>
2016-04-27 13:05:43 +0800

    contitech爬虫完工

--- src/main/java/com/hao/fetcher/FetcherContitech.java
+++ src/main/java/com/hao/fetcher/FetcherContitech.java
@@ -7,11 +7,15 @@ import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
 import com.hao.util.HttpClientUtil;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import static com.hao.common.Commons.*;
-import java.util.Map;
-import java.util.regex.Pattern;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Comparator;
+import java.util.Map;
 import static com.hao.common.Commons.*;
 
 /**
@@ -20,15 +24,26 @@ import static com.hao.common.Commons.*;
  */
 public class FetcherContitech {
 
-//    private static Pattern pattern = Pattern.compile("")
 
     public static Map<String,String> brands() {
         String url = "http://aam-china.contitech.de/pages/web-katalog/web-katalog_cn.cshtml";
         Document content = getDocument(url, "UTF-8");
         Elements optElems = content.select("#Select1_Markenauswahl").select("option");
-        Map<String,String> brands = Maps.newLinkedHashMap();
+        Map<String,String> brands = Maps.newTreeMap(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int i1 = Integer.parseInt(o1);
+                int i2 = Integer.parseInt(o2);
+                if (i1 > i2) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        });
         optElems.stream().filter(optElem -> !optElem.attr("value").equals("0")).forEach(optElem -> brands.put(optElem.attr("value"),optElem.text()));
-        System.out.println(brands);
+//        System.out.println(brands);
+
         return brands;
     }
 
@@ -38,37 +53,41 @@ public class FetcherContitech {
         Map<String,String> payloadMap = Maps.newLinkedHashMap();
         payloadMap.put("HerstellerID",herstellerID);
         String content = content(url,payloadMap);
-        content = content.replace("\\","");
-
-        System.out.println(content);
-        Map<String,String> map = Maps.newLinkedHashMap();
-//        JSONObject jsonObject = JSON.parseObject(content,);
-//        System.out.println(jsonObject);
-//        JSONArray jsonArray = jsonObject.getJSONArray("d");
-//        for (int i = 0; i < jsonArray.size(); i++) {
-//            JSONArray data = jsonArray.getJSONArray(i);
-//            map.put(data.getString(0),data.getString(1));
-//        }
-//        System.out.println(map);
-        return map;
+        Map<String, String> modelMap = parseContent(content);
+//        System.out.println(modelMap);
+        return modelMap;
     }
 
-    public static void types(String modelId) {
+    public static Map<String, String> types(String modelId) {
         String url = "http://aam-china.contitech.de/pages/web-katalog/WebService.asmx/FahrzeugsucheTypenAuslesen";
         Map<String,String> payloadMap = Maps.newLinkedHashMap();
         payloadMap.put("ModellID",modelId);
         String content = content(url,payloadMap);
-        content = content.replace("\\\\", "");
-        System.out.println(content);
+        Map<String, String> typeMap = parseContent(content);
+//        System.out.println(typeMap);
+        return typeMap;
     }
 
-    public static void art(String typId) {
+    public static Map<String, String> art(String typId) {
         String url = "http://aam-china.contitech.de/pages/web-katalog/WebService.asmx/FahrzeugsucheArtikelAuslesen";
         Map<String,String> payloadMap = Maps.newLinkedHashMap();
         payloadMap.put("TypID",typId);
         String content = content(url, payloadMap);
-        content = content.replace("\\","");
-        parseContent(content);
+        Map<String, String> typeMap = parseContent(content);
+        Map<String,String> urlMap = Maps.newLinkedHashMap();
+        typeMap.forEach((k,v) -> {
+            String val = v.substring(0,v.indexOf("\""));
+            try {
+                val = URLEncoder.encode(val,"UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            String specUrl = "http://aam-china.contitech.de/pages/pic/produkte.cshtml?lg=cn&applg=cn&ArtikelID=" + val;
+            urlMap.put(specUrl,v);
+        });
+        typeMap.putAll(urlMap);
+//        System.out.println(typeMap);
+        return typeMap;
     }
 
 
@@ -81,32 +100,78 @@ public class FetcherContitech {
         return content;
     }
 
-    public static void parseContent(String json) {
+    public static Map<String,String> parseContent(String json) {
         json = json.replace("\\","");
+        json = json.replace(":\"",":").replace("\"}","}");
+        Map<String,String> map = Maps.newLinkedHashMap();
         JSONObject jsonObject = JSON.parseObject(json);
         JSONArray jsonArray = jsonObject.getJSONArray("d");
-
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONArray data = jsonArray.getJSONArray(i);
+            String value = "";
+            for (int k = 1; k < data.size(); k++) {
+                if (k == 1) {
+                    value += data.getString(k) + "\"" + ",";
+                } else {
+                    value += "\"" + data.getString(k) + "\"" + ",";
+                }
+            }
+            map.put(data.getString(0),value);
+        }
+        return map;
     }
 
-    public static void spec(String url) {
+    public static String spec(String url) {
         Document document = getDocument(url, "UTF-8");
         Elements trElems = document.select("#Technischedaten > tbody > tr");
         String imgSrc = document.select("#StuecklistenHauptbild > img").attr("src");
         String img = "http://aam-china.contitech.de/pages/pic/" + imgSrc;
-        System.out.println(img);
-        trElems.forEach(trElem -> System.out.println(trElem));
+        String val = "";
+        for (Element trElem : trElems) {
+            Elements tdElems = trElem.select("td");
+            for (Element tdElem : tdElems) {
+                val += "\"" + tdElem.text() + "\"" + ",";
+            }
+        }
+        val += "\"" + img + "\"" + ",";
+        return val;
     }
 
-    public static void execute() {
-
+    public static void execute(String filePath) throws IOException {
+        Map<String, String> brandMap = brands();
+        for (Map.Entry<String,String> brandEntry : brandMap.entrySet()) {
+            Map<String, String> modelMap = models(brandEntry.getKey());
+            for (Map.Entry<String,String> modelEntry : modelMap.entrySet()) {
+                Map<String, String> typeMap = types(modelEntry.getKey());
+                for (Map.Entry<String,String> typeEntry : typeMap.entrySet()) {
+                    Map<String, String> artMap = art(typeEntry.getKey());
+                    for (Map.Entry<String,String> artEntry : artMap.entrySet()) {
+                        String data = "";
+                        if (artEntry.getKey().startsWith("http:")) {
+                            data = spec(artEntry.getKey());
+                        }else{
+                            continue;
+                        }
+                        String value = "\"" + brandEntry.getValue() + "\"" + "," +
+                                "\"" + modelEntry.getValue()  +
+                                "\""+typeEntry.getValue() +
+                                "\"" + artEntry.getValue() +
+                                data;
+                        writeStringtoFile(filePath,value + "\n",true);
+                        System.out.println(value);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
 //        brands();
-        models("99");
-//        types("");
+//        models("99");
+//        types("1425");
 //        art("5146");
 //        spec("http://aam-china.contitech.de/pages/pic/produkte.cshtml?lg=cn&applg=cn&ArtikelID=AVX10X800");
+        execute("D:/tmp/tech0427.txt");
     }
 
 }
